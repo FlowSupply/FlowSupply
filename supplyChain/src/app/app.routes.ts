201c9affef9a922b20982e06dcd1be2d0c8b69e8
Kris2327 <117087003+Kris2327@users.noreply.github.com>
2026-03-16 23:50:07 +0200

    neshto

--- supplyChain/src/app/app.routes.ts
+++ supplyChain/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import {SignUp} from './pages/sign-up/sign-up';
+//import { LogIn } from './pages/log-in/log-in';
 
 export const routes: Routes = [
-    { path: '', component: SignUp}
+    { path: 'signup', component: SignUp },
+    //{ path: 'login', component: LogIn}
 ];
