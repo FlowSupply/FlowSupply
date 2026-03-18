341f347a94123a2bfaa3e9a2279e06cee44a19c9
Kris2327 <117087003+Kris2327@users.noreply.github.com>
2026-03-18 17:20:44 +0200

    navbar

--- supplyChain/src/app/app.routes.ts
+++ supplyChain/src/app/app.routes.ts
@@ -2,10 +2,18 @@ import { Routes } from '@angular/router';
 
 import { SignUp } from './pages/sign-up/sign-up';
 import { LogIn } from './pages/log-in/log-in';
-
+import { Sidenavbar } from './layouts/sidenavbar/sidenavbar';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Suppliers } from './pages/suppliers/suppliers';
 
 export const routes: Routes = [
     { path: 'signup', component: SignUp },
-    { path: 'login', component: LogIn}
+    { path: 'signin', component: LogIn},
+    { path: '', component: Sidenavbar, 
+        children: [
+            { path: '', redirectTo: 'dashboard',  pathMatch: 'full'},
+            { path: 'dashboard', component: Dashboard},
+            { path: 'suppliers', component: Suppliers}
+    ]}
 ];
 
---
+++ supplyChain/src/app/layouts/sidenavbar/sidenavbar.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLinkWithHref, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidenavbar',
+  imports: [RouterOutlet, RouterLinkWithHref, CommonModule],
+  templateUrl: './sidenavbar.html',
+  styleUrl: './sidenavbar.css',
+})
+export class Sidenavbar {
+  activeLink: string = '';
+
+  setActive(link: string) {
+    this.activeLink = link;
+  }
+
+  constructor(private router: Router) {}
+
+  isActive(url: string) {
+    return this.router.url === url;
+  }
+}
