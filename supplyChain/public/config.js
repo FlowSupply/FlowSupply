0d881dca70ab78cf172028e5963a10db244a7c38
Emil Emilov Ignatov <emkata@ma6ina.local>
2026-05-09 02:04:04 +0300

    Point frontend to active Render API

--- supplyChain/public/config.js
+++ supplyChain/public/config.js
@@ -1,1 +1,1 @@
-window.__FLOW_SUPPLY_API_BASE_URL__ = window.__FLOW_SUPPLY_API_BASE_URL__ || 'https://flowsupply-api.onrender.com/api';
+window.__FLOW_SUPPLY_API_BASE_URL__ = window.__FLOW_SUPPLY_API_BASE_URL__ || 'https://flowsupply-api-c7c9.onrender.com/api';
